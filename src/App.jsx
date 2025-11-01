5633d4baec056ecbe74cff5c8c77870115a95803
GOPIKA K <gopikagopus2025@gmail.com>
2025-11-01 15:47:47 +0530

    Save current changes before switching to backup branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,16 @@
-import './App.css'
-import React from 'react'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/login";
+import Register from "./pages/register";
 
 function App() {
   return (
-    <>
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
