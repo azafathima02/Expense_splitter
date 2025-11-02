380fa8781c00a99a67623a46402779ae631cc141
AmeghM <ameghm407@gmail.com>
2025-11-02 19:35:09 +0530

    dash

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,10 +1,9 @@
 // Dashboard.jsx
-import React from "react";
 
 export default function Dashboard() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-black text-yellow-400 text-3xl font-bold">
-      Welcome to the Dashboard 
+      Welcome to the Dashboard
     </div>
   );
 }
