b8987bebe9df9b52209e0619da8a495d8756a793
AmeghM <ameghm407@gmail.com>
2025-11-04 00:02:52 +0530

    updated app paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/login";
 import Register from "./pages/register";
 import Dashboard from "./pages/dashboard";
 import Expenses from "./pages/Expenses";
+import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar";
 
 function App() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/expenses" element={<Expenses />} /> {/* new route */}
+      <Route
+        path="/dashboard"
+        element={
+          <div className="flex min-h-screen bg-gray-100">
+            <Sidebar
+              isSidebarOpen={isSidebarOpen}
+              toggleSidebar={toggleSidebar}
+            />
+            <div className="flex-1">
+              <Navbar toggleSidebar={toggleSidebar} />
+              <div className="p-4">
+                <Dashboard />
+              </div>
+            </div>
+          </div>
+        }
+      />
+      <Route
+        path="/expenses"
+        element={
+          <div className="flex min-h-screen bg-gray-100">
+            <Sidebar
+              isSidebarOpen={isSidebarOpen}
+              toggleSidebar={toggleSidebar}
+            />
+            <div className="flex-1">
+              <Navbar toggleSidebar={toggleSidebar} />
+              <div className="p-4">
+                <Expenses />
+              </div>
+            </div>
+          </div>
+        }
+      />
+      {/* new route */}
     </Routes>
   );
 }
